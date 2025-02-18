1f88b6a4744e8e636b21b9bdb16f72c1a6ef96f0
JianYang98 <daibobby1221@gmail.com>
2025-02-19 00:52:58 +0900

    내용 : main 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/url/UrlShortenController.java
+++ src/main/java/community/whatever/onembackendjava/url/UrlShortenController.java
@@ -1,36 +1,23 @@
 package community.whatever.onembackendjava.url;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class UrlShortenController {
 
-
     private  final UrlShortenService urlShortenService ;
 
-    @Autowired
     private UrlShortenController(UrlShortenService urlShortenService){
         this.urlShortenService = urlShortenService ;
     }
-
-    @PostMapping("/shorten-url/search")
-    public String shortenUrlSearch(@RequestBody RequestVO requestVO ) {
-        return urlShortenService.searchUrl(requestVO.getKey());
-    }
-    @PostMapping("/shorten-url/create")
-    public String shortenUrlCreate(@RequestBody RequestVO requestVO ) {
-        return  urlShortenService.createKey(requestVO.getUrl()) ;
+    @GetMapping("/shorten-url/{key}")
+    public String getUrlByKey(@PathVariable String key) {
+        return urlShortenService.getUrlByKey(key);
     }
 
-    @GetMapping("/shorten-url/delete/{key}")
-    public String shortenUrlDelete(@PathVariable String key){
-    //  delete에 대하여 return 메세지 고민 즁
-        String result ="False" ;
-        if(urlShortenService.deleteKey(key)){
-            result = "True";
-        }
-        return result;
-
+    @PostMapping("/shorten-url")
+    public String shortenUrlCreate(@RequestBody ShortenUrlRequest request) {
+        return urlShortenService.createKey(request.originUrl());
     }
 }
